80fb42ae88122611f2e0cbc8ba791aa30e25dab4
CJCM'S laptops <cjcastillomendoza@gmail.com>
2026-06-21 23:47:24 -0500

    anotaciones spring para la entidad Loan

---
+++ src/main/java/com/cjcm/bibliotecaApp/model_domain/LoanEntity.java
@@ -0,0 +1,15 @@
+package com.cjcm.bibliotecaApp.model_domain;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "loans")
+@Getter @Setter
+@NoArgsConstructor
+public class LoanEntity {
+
+  
+}
