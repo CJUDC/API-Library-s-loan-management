1f5e7449417ac3d3ef9f030fb9134e054ccc922c
Cjcm <ccastillom1@gmail.com>
2026-06-23 00:04:52 -0500

    feat: Implementación endpoint getAllLoans

---
+++ src/main/java/com/cjcm/bibliotecaApp/controller/LoanController.java
@@ -0,0 +1,28 @@
+package com.cjcm.bibliotecaApp.controller;
+
+import com.cjcm.bibliotecaApp.model_domain.LoanEntity;
+import com.cjcm.bibliotecaApp.service.LoanService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/loans")
+public class LoanController {
+
+  private final LoanService loanService;
+
+  public LoanController(LoanService loanService) {
+    this.loanService = loanService;
+  }
+
+  @GetMapping
+  public ResponseEntity<List<LoanEntity>> getAllLoans() {
+    List<LoanEntity> loans = loanService.getAllLoans();
+    return ResponseEntity.ok(loans);
+  }
+
+}
