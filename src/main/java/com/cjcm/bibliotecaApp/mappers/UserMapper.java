551e9d732109d571250a4f752332c4ff0946f021
CJCM'S laptops <cjcastillomendoza@gmail.com>
2026-06-25 01:15:48 -0500

    feat: Creación método mapToUserResponseDto

---
+++ src/main/java/com/cjcm/bibliotecaApp/mappers/UserMapper.java
@@ -0,0 +1,24 @@
+package com.cjcm.bibliotecaApp.mappers;
+
+import com.cjcm.bibliotecaApp.dto.UserRequestDto;
+import com.cjcm.bibliotecaApp.dto.UserResponseDto;
+import com.cjcm.bibliotecaApp.persistence.entities.UserEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+//  public UserEntity mapToUserEntity(UserRequestDto userRequestDto){
+//
+//  } TODO
+
+  public static UserResponseDto mapToUserResponseDto(UserEntity userEntity) {
+    // Implementation for mapping UserEntity to UserResponseDto
+    return UserResponseDto.builder()
+            .id(userEntity.getUserId())
+            .email(userEntity.getEmail())
+            .name(userEntity.getName())
+            .lastName(userEntity.getLastName())
+            .build();
+  }
+}
