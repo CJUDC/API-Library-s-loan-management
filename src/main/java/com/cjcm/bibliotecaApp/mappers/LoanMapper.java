a9369fb621fba80c4b8a82539ec23bf7f04a65cb
Cjcm <ccastillom1@gmail.com>
2026-06-26 01:27:14 -0500

    feat: Implementación del mapper dentro del builder de LoanResponseDto en .loanbooks

--- src/main/java/com/cjcm/bibliotecaApp/mappers/LoanMapper.java
+++ src/main/java/com/cjcm/bibliotecaApp/mappers/LoanMapper.java
@@ -1,23 +1,43 @@
 package com.cjcm.bibliotecaApp.mappers;
 
+import com.cjcm.bibliotecaApp.dto.loanDtos.LoanRequestDto;
 import com.cjcm.bibliotecaApp.dto.loanDtos.LoanResponseDto;
+import com.cjcm.bibliotecaApp.persistence.entities.LoanBookEntity;
 import com.cjcm.bibliotecaApp.persistence.entities.LoanEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LoanMapper {
 
-  public LoanResponseDto mapToLoanDto(LoanEntity  loanEntity){
+  private final LoanBookMapper loanBookMapper;
+
+  public LoanMapper(LoanBookMapper loanBookMapper) {
+    this.loanBookMapper = loanBookMapper;
+  }
+
+  public LoanResponseDto mapToLoanDto(LoanEntity loanEntity){
     return LoanResponseDto.builder()
             .loanId(loanEntity.getLoanId())
             .startDate(loanEntity.getStartDate())
             .expireDate(loanEntity.getExpireDate())
             .returnDate(loanEntity.getReturnDate())
             .state(loanEntity.getState())
-            .loanBooks(loanEntity.getLoanBooks())
+            .loanBooks(loanBookMapper.mapLoanBooksToDto(loanEntity.getLoanBooks()))
             .build();
   }
 
+  public LoanEntity mapToLoanEntity(LoanRequestDto loanRequestDto){
 
+    LoanEntity loanEntity = new LoanEntity();
+
+    loanEntity.setLoanId(loanRequestDto.loanId());
+    loanEntity.setStartDate(loanRequestDto.startDate());
+    loanEntity.setExpireDate(loanRequestDto.expireDate());
+    loanEntity.setReturnDate(loanRequestDto.returnDate());
+    loanEntity.setState(loanRequestDto.state());
+    loanEntity.setLoanBooks(loanRequestDto.loanBooks());
+
+    return loanEntity;
+  }
 
 }
